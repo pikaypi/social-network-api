be60b63d2375fc596999ec4de617f921796aa616
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-11 12:32:28 -0500

    add virtual and date format to thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,43 +1,60 @@
 const { Schema, model } = require('mongoose');
+const format_date = require('../utils/helpers');
 
-const reactionSchema = new Schema({
-    reactionId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Schema.Types.ObjectId()
-    },
-    reactionBody: {
-        type: String,
-        require: true,
-        minLength: 1,
-        maxLength: 280
-    },
-    username: {
-        type: String,
-        require: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Schema.Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            require: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            require: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
     }
-});
+);
 
-const thoughtSchema = new Schema({
-    thoughtText: {
-        type: String,
-        require: true,
-        minLength: 1,
-        maxLength: 280
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            require: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: v => format_date(v)
+        },
+        username: {
+            type: String,
+            require: true
+        },
+        reactions: [reactionSchema]
     },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    username: {
-        type: String,
-        require: true
-    },
-    reactions: [reactionSchema]
-});
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length;
+    })
 
 const Thought = model('thought', thoughtSchema);
 
