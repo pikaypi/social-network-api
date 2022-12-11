a2d895309f49a62618559d2e97090692273344d8
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-11 11:50:27 -0500

    move reactionSchema before thoughSchema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,26 @@
 const { Schema, model } = require('mongoose');
 
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Schema.Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        require: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        require: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
 const thoughtSchema = new Schema({
     thoughtText: {
         type: String,
@@ -17,27 +38,6 @@ const thoughtSchema = new Schema({
     },
     reactions: [reactionSchema]
 });
-
-const reactionSchema = new Schema({
-    reactionId: {
-        type: Types.ObjectId,
-        default: () => new Schema.Types.ObjectId()
-    },
-    reactionBody: {
-        type: String,
-        require: true,
-        minLength: 1,
-        maxLength: 280
-    },
-    username: {
-        type: String,
-        require: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-})
 
 const Thought = model('thought', thoughtSchema);
 
