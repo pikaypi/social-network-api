e507dcf2e27bc06a30b290934cc2a317146f45fa
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-11 13:20:23 -0500

    route and controller to delete a user

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -22,5 +22,20 @@ module.exports = {
                     : res.status(200).json(user)
             )
             .catch((err) => res.status(500).json(err));
+    },
+    // Delete a single user
+    deleteUser(req, res) {
+        User.findOneAndRemove({ _id: req.params.userId })
+            .then((user) => 
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID'})
+                    : Thought.deleteMany({ username: user.username })
+            )
+            .then((thoughts) => 
+                !thoughts
+                    ? res.status(200).json({ message: 'User deleted but there were no thoughts to delete' })
+                    : res.status(200).json({ message: 'User and all user thoughts have been deleted' })
+            )
+            .catch((err) => res.status(500).json(err));
     }
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,13 +2,14 @@ const router = require('express').Router();
 const {
     getUsers,
     createUser,
-    getUser
+    getUser,
+    deleteUser
 } = require('../../controllers/userControllers');
 
 // endpoint: /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // endpoint /api/users/:userId
-router.route('/:userId').get(getUser);
+router.route('/:userId').get(getUser).delete(deleteUser);
 
 module.exports = router;
