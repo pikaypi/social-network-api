ffa172d7e2519f018d91231c9c9556f077103e78
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-11 12:04:33 -0500

    controller logic for getUsers

---
+++ controllers/userControllers.js
@@ -0,0 +1,10 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+    // Get all users
+    getUsers(req, res) {
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err))
+    }
+};
