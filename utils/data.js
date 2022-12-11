e10af6f3a8aa4bd23c09eceb6c28b8840ee1b5c5
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-11 11:50:45 -0500

    add seed data and seed function

---
+++ utils/data.js
@@ -0,0 +1,23 @@
+const users = [
+    {
+        username: 'pikaypi',
+        email: 'pikaypi@email.com'
+    },
+    {
+        username: 'explorer',
+        email: 'explorer@email.com'
+    }
+];
+
+const thoughts = [
+    {
+        thoughtText: 'Text of thought 1',
+        username: 'pikaypi'
+    },
+    {
+        thoughtText: 'Text of thought 2',
+        username: 'explorer'
+    }
+];
+
+module.exports = { thoughts, users };
